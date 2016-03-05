179752eebbe4b7fc795c00b6c1bd7773113c28e1
Craig Nielsen <craiglbn@gmail.com>
2016-03-05 14:27:18 +0200

    added routing

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 #setup perforce gitMergeTool
+echo "please make sure perforce merge tool .tgz is present in downloads, with correct version edited into this file"
 tar -xzf /home/craig/Downloads/p4v.tgz 
 sudo mv /home/craig/Downloads/p4v-2015.2.1315639 /opt/p4merge
 sudo ln -s /opt/p4merge/bin/p4merge /usr/local/bin/p4merge
