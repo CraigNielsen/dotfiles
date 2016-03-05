9aa4e545dc70b7d4123c4480f3e05684d5af9503
Craig Nielsen <craiglbn@gmail.com>
2016-03-05 18:37:38 +0200

    symlink edit

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 #setup perforce gitMergeTool
 echo "please make sure perforce merge tool .tgz is present in downloads, with correct version edited into this file"
 tar -xzf /home/craig/Downloads/p4v.tgz 
-sudo mv /home/craig/Downloads/p4v-2015.2.1315639 /opt/p4merge
+sudo mv ./p4v-2015.2.1315639 /opt/p4merge
 sudo ln -s /opt/p4merge/bin/p4merge /usr/local/bin/p4merge
 #copy scripts to home
 cp .gitconfig ~/.gitconfig
