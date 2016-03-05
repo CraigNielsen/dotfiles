3dd63a615b8f4840a64b77e44547d800efa17d5c
Craig Nielsen <craiglbn@gmail.com>
2016-03-05 14:26:04 +0200

    added routing

--- setup.sh
+++ setup.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 #setup perforce gitMergeTool
+tar -xzf /home/craig/Downloads/p4v.tgz 
+sudo mv /home/craig/Downloads/p4v-2015.2.1315639 /opt/p4merge
+sudo ln -s /opt/p4merge/bin/p4merge /usr/local/bin/p4merge
 #copy scripts to home
 cp .gitconfig ~/.gitconfig
 cp .zpreztorc ~/.zpreztorc
