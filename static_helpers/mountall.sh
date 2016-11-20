f70d6ceff691028ab1694fa2fb4ccbced37bf38c
Craig Nielsen <craiglbn@gmail.com>
2016-11-20 16:17:33 +0200

    Delete old mount locations from mountall.sh

--- static_helpers/mountall.sh
+++ static_helpers/mountall.sh
@@ -1,4 +1,2 @@
 sudo mount -t ntfs /dev/sda1 /media/SSDwindows
-sudo mount -t ntfs /dev/sdc2 /media/windowspart
-sudo mount -t ntfs /dev/sdc3 /media/swap
-sudo mount -t ntfs /dev/sdb1 /media/Memory
+sudo mount -t ntfs /dev/sdc1 /media/Memory
