7dbac987fc5b156655d45867a45d90b4dcceaa13
Craig Nielsen <craiglbn@gmail.com>
2016-12-20 08:48:00 +0200

    New savestate approach to machine config

---
+++ backups/saveState.sh
@@ -0,0 +1,23 @@
+#!/bin/zsh
+cd /home/craig/.config/nvim/UltiSnips
+git add .
+git commit
+echo 'pushing UltiSnips'
+git push
+cd /home/craig/vim_projects
+git add .
+git commit
+echo 'pushing vim_projects'
+git push
+cd /home/craig/.config/nvim/config
+git add .
+git commit
+echo 'pushing vim config files'
+git push
+cd /home/craig/git_repos/dotfiles
+python saveConfig.py
+git add .
+git commit
+echo 'pushing vim config files'
+git push
+
