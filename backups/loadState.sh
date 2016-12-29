ec1b1b8aca1bb3e55ef99a6e71ddea60adb2dd4c
Craig Nielsen <craiglbn@gmail.com>
2016-12-29 08:34:10 +0200

    Not sure

---
+++ backups/loadState.sh
@@ -0,0 +1,11 @@
+#!/bin/zsh
+cd /home/craig/.config/nvim/UltiSnips
+echo 'pulling UltiSnips'
+git pull
+cd /home/craig/vim_projects
+echo 'pulling vim_projects'
+git pull
+cd /home/craig/.config/nvim/config
+echo 'pulling vim config files'
+git pull
+
