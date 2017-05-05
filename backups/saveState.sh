7ba206432b431110ccfa14a2d667bdc0240ff68c
Craig Nielsen <craiglbn@gmail.com>
2017-05-05 09:40:41 +0200

    some more errors

--- backups/saveState.sh
+++ backups/saveState.sh
@@ -1,45 +1,45 @@
 #!/bin/zsh
-cd $PWD/git_repos/dotfiles;
+cd $HOME/git_repos/dotfiles;
 
-if [ $PWD=='/home/craig' ]
+if [ $HOME=='/home/craig' ]
 then
   echo "in debian"
   python saveConfig.py debSave.txt
-elif [ $PWD=='/craig.ferguson' ]
+elif [ $HOME=='/craig.ferguson' ]
 then
   echo "in mac"
   python saveConfig.py macSave.txt
 fi
 
-cd $PWD/.config/nvim/UltiSnips
+cd $HOME/.config/nvim/UltiSnips
 git add .
 git commit
 echo 'pushing UltiSnips'
 git push
-cd $PWD/vim_projects
+cd $HOME/vim_projects
 git add .
 git commit
 echo 'pushing vim_projects'
 git push
 #_____
-cd $PWD/workspace/cdev
+cd $HOME/workspace/cdev
 git add .
 git commit
 echo 'pushing takealot dev files'
 git push
 #_____
-cd $PWD/.config/nvim/config
+cd $HOME/.config/nvim/config
 git add .
 git commit
 echo 'pushing vim config files'
 git push
-cd $PWD/git_repos/dotfiles
+cd $HOME/git_repos/dotfiles
 python saveConfig.py
 git add .
 git commit
 echo 'pushing dotfiles files'
 git push
-cd $PWD/git_repos/craig_help
+cd $HOME/git_repos/craig_help
 git add .
 git commit
 echo 'pushing craig_help files'
