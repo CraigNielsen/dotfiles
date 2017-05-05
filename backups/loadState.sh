00bb3f8d73c7211ede939d9ee3a05f0a62f87eec
Craig Ferguson <craig.ferguson@takealot.com>
2017-05-05 07:54:09 +0200

    Update saveStates

--- backups/loadState.sh
+++ backups/loadState.sh
@@ -1,17 +1,17 @@
 #!/bin/zsh
-cd $1/.config/nvim/UltiSnips
+cd $PWD/.config/nvim/UltiSnips
 echo 'pulling UltiSnips'
 git pull
-cd $1/vim_projects
+cd $PWD/vim_projects
 echo 'pulling vim_projects'
 git pull
-cd $1/.config/nvim/config
+cd $PWD/.config/nvim/config
 echo 'pulling vim config files'
 git pull
-cd $1/git_repos/dotfiles
+cd $PWD/git_repos/dotfiles
 echo 'pulling dotfiles'
 git pull
 #_____
-cd $1/workspace/cdev
+cd $PWD/workspace/cdev
 git pull
 echo 'pulling takealot dev files'
--- backups/saveState.sh
+++ backups/saveState.sh
@@ -1,27 +1,29 @@
 #!/bin/zsh
-cd $1/.config/nvim/UltiSnips
+cd $PWD/git_repos/dotfiles
+python saveConfig.py $1
+cd $PWD/.config/nvim/UltiSnips
 git add .
 git commit
 echo 'pushing UltiSnips'
 git push
-cd $1/vim_projects
+cd $PWD/vim_projects
 git add .
 git commit
 echo 'pushing vim_projects'
 git push
 #_____
-cd $1/workspace/cdev
+cd $PWD/workspace/cdev
 git add .
 git commit
 echo 'pushing takealot dev files'
 git push
 #_____
-cd $1/.config/nvim/config
+cd $PWD/.config/nvim/config
 git add .
 git commit
 echo 'pushing vim config files'
 git push
-cd $1/git_repos/dotfiles
+cd $PWD/git_repos/dotfiles
 python saveConfig.py
 git add .
 git commit
